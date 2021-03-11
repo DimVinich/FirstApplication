ed5ca95319fde59bfb5bcccd2a713b48fbfd5199
DimVinich <DmitrijVS@gmail.com>
2021-03-11 20:12:48 +0300

    После раздела 3,5,2

--- FirstApplication/Program.cs
+++ FirstApplication/Program.cs
@@ -4,26 +4,16 @@ class MainClass
 {
 	public static void Main(string[] args)
 	{
-
-		string MyName = "Евгения";
-		Console.WriteLine("Привет, мир");
-		Console.WriteLine();
+		string MyName = "Computer";
+		Console.WriteLine("Привет, человек");
 		Console.WriteLine("Меня зовут {0}", MyName);
+		Console.Write("А как тебя зовут? ");
+		string YourName = Console.ReadLine();
+		Console.Write("Рад встрече, {0} ", YourName);
 
-		//0 — позиция нашей переменной. То есть переменная будет на позиции 0.
+		string str = Console.ReadLine();
 
 
-		//Также мы можем вставить в строку не переменную, а строковый литерал:
-		Console.WriteLine("Меня зовут {0}", "Маша");
-
-
-		//И, естественно, мы можем использовать несколько переменных при выводе. В этом случае располагаем их в порядке нумерации «мест».
-		var age = 27;
-		var weight = 50;
-		var favcolor = "black";
-		var name = "Jane";
-		Console.WriteLine("Возраст {0} \n Вес {1} \n favcolor {2}", age, weight, favcolor);
-		Console.WriteLine("{0} \n {1} \n {2}", name, age, favcolor);
 		Console.ReadKey();
 	}
 }
