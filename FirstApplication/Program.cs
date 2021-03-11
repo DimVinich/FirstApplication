2b47dbe18bb1336bc401f6cbea1d9e1e1942df72
DimVinich <DmitrijVS@gmail.com>
2021-03-11 20:09:06 +0300

    Псле раздела 3,3

--- FirstApplication/Program.cs
+++ FirstApplication/Program.cs
@@ -5,15 +5,15 @@ class MainClass
 	public static void Main(string[] args)
 	{
 
-		const string MyName = "Jane";
+		string MyName = "Jane";
+		byte MyAge = 27;
+		bool HaveIApet = true;
+		double MyShoeSize = 37.5;
 
-		Console.WriteLine(MyName);
-		Console.WriteLine("\tПривет, мир");
-		Console.WriteLine("\tМне 27 лет");
-		Console.WriteLine("\t My name is \n {0}", MyName);
-		Console.WriteLine("\u0040");
-		Console.WriteLine("\u0023");
+		Console.WriteLine("My name is " + MyName);
+		Console.WriteLine("MyAge " + MyAge);
+		Console.WriteLine("Do I have a pet? " + HaveIApet);
+		Console.WriteLine("My shoe size is " + MyShoeSize);
 
-		Console.ReadKey();
 	}
 }
