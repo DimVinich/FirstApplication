245da65156eb47a76f1adbf3bd73718b0b2c0a2e
DimVinich <DmitrijVS@gmail.com>
2021-03-11 20:17:21 +0300

    Финальное 3 раздела

--- FirstApplication/Program.cs
+++ FirstApplication/Program.cs
@@ -4,16 +4,21 @@ class MainClass
 {
 	public static void Main(string[] args)
 	{
-
-		//var name = "Jane";
-		Console.Write("Введите имя: ");
+		Console.Write("Enter your name: ");
 		var name = Console.ReadLine().ToString();
 
-		Console.Write("Введите возраст: ");
-		var age = checked((byte)int.Parse(Console.ReadLine()));
-		Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+		Console.Write("Enter your age: ");
+		var age = Convert.ToInt32(Console.ReadLine());
 
-		//Console.Write("What is your favorite day of week? ");
+		Console.WriteLine("Your name is {0} and age is {1}", name, age);
 
-//var day = (DayOfWeek) int.Parse(Console.ReadLine());
-//Console.WriteLine("Your favorite day is {0}", day);
+		Console.Write("Enter your birthdate: ");
+		var birthdate = Console.ReadLine();
+
+		Console.WriteLine("Your birthdate is " + birthdate);
+
+		Console.ReadKey();
+		//Console.Write("");
+		//Console.ReadLine();
+	}
+}
