dd921fef333a4d08065b4220c089ef2e8e97ce5e
DimVinich <DmitrijVS@gmail.com>
2021-03-11 20:12:13 +0300

    Раздел 3,5,1

--- FirstApplication/Program.cs
+++ FirstApplication/Program.cs
@@ -4,25 +4,26 @@ class MainClass
 {
 	public static void Main(string[] args)
 	{
-		DaysOfWeek MyFavoriteDay;
 
-		MyFavoriteDay = DaysOfWeek.Friday;
+		string MyName = "Евгения";
+		Console.WriteLine("Привет, мир");
+		Console.WriteLine();
+		Console.WriteLine("Меня зовут {0}", MyName);
 
-		Console.WriteLine(MyFavoriteDay);
+		//0 — позиция нашей переменной. То есть переменная будет на позиции 0.
+
+
+		//Также мы можем вставить в строку не переменную, а строковый литерал:
+		Console.WriteLine("Меня зовут {0}", "Маша");
+
+
+		//И, естественно, мы можем использовать несколько переменных при выводе. В этом случае располагаем их в порядке нумерации «мест».
+		var age = 27;
+		var weight = 50;
+		var favcolor = "black";
+		var name = "Jane";
+		Console.WriteLine("Возраст {0} \n Вес {1} \n favcolor {2}", age, weight, favcolor);
+		Console.WriteLine("{0} \n {1} \n {2}", name, age, favcolor);
+		Console.ReadKey();
 	}
-}
-
-enum DaysOfWeek : byte
-{
-	Tuesday,
-	Monday,
-	Wednesday,
-	Friday
-}
-
-enum Semaphore : int
-{
-	Red = 100,
-	Yellow = 200,
-	Green = 300
 }
