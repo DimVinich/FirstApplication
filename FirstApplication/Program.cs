da0878f1d9508b1c6423f7d4b3457ca36244dc09
DimVinich <DmitrijVS@gmail.com>
2021-03-11 20:11:03 +0300

    После раздела 3,4

--- FirstApplication/Program.cs
+++ FirstApplication/Program.cs
@@ -2,13 +2,27 @@
 
 class MainClass
 {
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("IntMin {0} ", int.MinValue);
-        Console.WriteLine("IntMax {0} ", int.MaxValue);
-        Console.WriteLine("LongMin {0} ", long.MinValue);
-        Console.WriteLine("LongMax {0} ", long.MaxValue);
-        Console.WriteLine("DecimalMax {0} ", decimal.MaxValue);
-        Console.WriteLine("DoubleMax {0} ", double.MaxValue);
-    }
+	public static void Main(string[] args)
+	{
+		DaysOfWeek MyFavoriteDay;
+
+		MyFavoriteDay = DaysOfWeek.Friday;
+
+		Console.WriteLine(MyFavoriteDay);
+	}
+}
+
+enum DaysOfWeek : byte
+{
+	Tuesday,
+	Monday,
+	Wednesday,
+	Friday
+}
+
+enum Semaphore : int
+{
+	Red = 100,
+	Yellow = 200,
+	Green = 300
 }
