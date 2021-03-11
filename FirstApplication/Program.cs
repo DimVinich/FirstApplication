5782bdbb3be5d335e6114cd4c76cd95803b9006a
DimVinich <DmitrijVS@gmail.com>
2021-03-11 20:10:21 +0300

    Задание по мин.  и макс. значениям

--- FirstApplication/Program.cs
+++ FirstApplication/Program.cs
@@ -2,18 +2,13 @@
 
 class MainClass
 {
-	public static void Main(string[] args)
-	{
-
-		string MyName = "Jane";
-		byte MyAge = 27;
-		bool HaveIApet = true;
-		double MyShoeSize = 37.5;
-
-		Console.WriteLine("My name is " + MyName);
-		Console.WriteLine("MyAge " + MyAge);
-		Console.WriteLine("Do I have a pet? " + HaveIApet);
-		Console.WriteLine("My shoe size is " + MyShoeSize);
-
-	}
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("IntMin {0} ", int.MinValue);
+        Console.WriteLine("IntMax {0} ", int.MaxValue);
+        Console.WriteLine("LongMin {0} ", long.MinValue);
+        Console.WriteLine("LongMax {0} ", long.MaxValue);
+        Console.WriteLine("DecimalMax {0} ", decimal.MaxValue);
+        Console.WriteLine("DoubleMax {0} ", double.MaxValue);
+    }
 }
