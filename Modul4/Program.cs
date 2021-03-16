ea8c7d81a89b428b2b5178ea57bfb4a2b1998931
DimVinich <DmitrijVS@gmail.com>
2021-03-16 16:21:01 +0300

    Задание 4,1,17

--- Modul4/Program.cs
+++ Modul4/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
-namespace Modul4
+class MainClass
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	public static void Main(string[] args)
+	{
+		// ===============================		Задание 4,1,17
+		Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+		var color = Console.ReadLine();
+
+		if (color == "red")
+		{
+			Console.BackgroundColor = ConsoleColor.Red;
+			Console.ForegroundColor = ConsoleColor.Black;
+
+			Console.WriteLine("Your color is red!");
+		}
+
+		else if (color == "green")
+		{
+			Console.BackgroundColor = ConsoleColor.Green;
+			Console.ForegroundColor = ConsoleColor.Black;
+
+			Console.WriteLine("Your color is green!");
+		}
+		else
+		{
+			Console.BackgroundColor = ConsoleColor.Cyan;
+			Console.ForegroundColor = ConsoleColor.Black;
+
+			Console.WriteLine("Your color is cyan!");
+		}
+	}
 }
