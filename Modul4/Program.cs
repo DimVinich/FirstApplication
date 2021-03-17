e83ff0d41100977145902b41b36c6feab620b7a8
DimVinich <DmitrijVS@gmail.com>
2021-03-17 20:04:03 +0300

    Задание 4.3.17

--- Modul4/Program.cs
+++ Modul4/Program.cs
@@ -4,27 +4,44 @@ class MainClass
 {
     public static void Main(string[] args)
 
-    // ===================================== Задание 4.3.16
+    // ===================================== Задание 4.3.17
     
     {
         int[,] arr = { { -5, 6, 9, 1, 2, -3 } , { -8, 8, 1, 1, 2, -3 } };
         int posNumber = 0;
 
-        foreach (var arrItem in arr)
-        {
-            posNumber = arrItem > 0 ? ++posNumber : posNumber;
-        }
-        Console.WriteLine("кол-во положительных чисел " + posNumber);
-
-
-        posNumber = 0;
-
         for (int i = 0; i < arr.GetUpperBound(0) +1; i++)
         {
-            for(int j = 0; j < arr.GetUpperBound(1) +1; j++)
-            posNumber = arr[i,j] > 0 ? ++posNumber : posNumber;
+            for (int j = 0; j < arr.GetUpperBound(1) + 1; j++)
+            {
+                Console.Write(arr[i, j] + " ");
+            }
+            Console.WriteLine();
         }
-        Console.WriteLine("кол-во положитеьных чисел "+posNumber);
+
+
+        //  =================== Сортировка
+        int temp = 0;
+        for (int k = 0; k <= arr.GetUpperBound(0); k++)
+        {
+            for (int i = 0; i <= arr.GetUpperBound(1); i++)
+            {
+                for (int j = i + 1; j <= arr.GetUpperBound(1); j++)
+                {
+                    if (arr[k,i] > arr[k,j])
+                    {
+                        temp = arr[k, i];
+                        arr[k, i] = arr[k, j];
+                        arr[k, j] = temp;
+                    }
+                }
+
+                Console.Write(arr[k, i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+
 
         //Console.WriteLine();
         //Console.Write(arr[i] + " ");
