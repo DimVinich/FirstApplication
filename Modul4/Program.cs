f5870513bfc441ea60a8e7a50c113b8c5ab22738
DimVinich <DmitrijVS@gmail.com>
2021-03-18 19:06:59 +0300

    Задание 4.4.3

--- Modul4/Program.cs
+++ Modul4/Program.cs
@@ -4,29 +4,22 @@ class MainClass
 {
     public static void Main(string[] args)
 
-    // ===================================== Задание 4.4.2 часть 2
-    //              Исходный код взят с финального здания модуля 3
+    // ===================================== Задание 4.4.3
     
     {
-        //var anketa = (name: "", age: 0, birthdate: "");
-        // Более правильное задание кортеджа
-        //(string name, int age, string birthdate) anketa;
+        var (name, age) = ("Dimon", 99);
 
-        // ещё одни вариант декларирования кортеджа
-        var (name, age, birthdate) = ("Dimon", 99, "32.13.2222");
+        Console.WriteLine("Моё имя: {0}", name);
+        Console.WriteLine("Мой возраст: {0}", age);
 
-        //Console.Write("Enter your name: ");
-        //anketa.name = Console.ReadLine().ToString();
+        Console.Write("Введите имя: ");
+        name = Console.ReadLine().ToString();
 
-        //Console.Write("Enter your age: ");
-        //anketa.age = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Ведите возраст с цифрами: ");
+        age = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Your name is {0} and age is {1}", name, age);
-
-        //Console.Write("Enter your birthdate: ");
-        //anketa.birthdate = Console.ReadLine();
-
-        Console.WriteLine("Your birthdate is " + birthdate);
+        Console.WriteLine("Ваше имя :{0}", name);
+        Console.WriteLine("Ваш возраст :{0}", age);
 
         //Console.WriteLine();
         //Console.Write(arr[i] + " ");
