260b8e5eb64cdba9bcd820c656ddfd7433998149
DimVinich <DmitrijVS@gmail.com>
2021-03-18 19:29:49 +0300

    Задание 4.4.5

--- Modul4/Program.cs
+++ Modul4/Program.cs
@@ -4,25 +4,29 @@ class MainClass
 {
     public static void Main(string[] args)
 
-    // ===================================== Задание 4.4.3
+    // ===================================== Задание 4.4.5
     
     {
-        var (name, age) = ("Dimon", 99);
+        (string Name, string Type, double Age, int NameCount) Pet;
 
-        Console.WriteLine("Моё имя: {0}", name);
-        Console.WriteLine("Мой возраст: {0}", age);
+        Console.Write("Введите имя питомца: ");
+        Pet.Name = Console.ReadLine();
 
-        Console.Write("Введите имя: ");
-        name = Console.ReadLine().ToString();
+        Console.Write("Ведите тип Вашего питомца (кошка, собака и т.д. ): ");
+        Pet.Type = (Console.ReadLine());
 
-        Console.Write("Ведите возраст с цифрами: ");
-        age = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Ведите возвраст Вашего питомца: ");
+        Pet.Age = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("Ваше имя :{0}", name);
-        Console.WriteLine("Ваш возраст :{0}", age);
+        Pet.NameCount = Pet.Name.Length;
 
-        //Console.WriteLine();
-        //Console.Write(arr[i] + " ");
+        Console.WriteLine("Имя Вашего питомца :{0}", Pet.Name);
+        Console.WriteLine("Тип вашего питомца :{0}", Pet.Type );
+        Console.WriteLine("Возраст вашего питомца :{0}", Pet.Age);
+
+
+        //Console.WriteLine("Моё имя: {0}", name);
+        //name = Console.ReadLine().ToString();
         Console.ReadKey();
     }
 
