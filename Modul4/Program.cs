82b04548f7aeba1c14236f2ce605f07aad65a885
DimVinich <DmitrijVS@gmail.com>
2021-03-18 14:25:46 +0300

    Задание 4.4.2 часть 2

--- Modul4/Program.cs
+++ Modul4/Program.cs
@@ -4,23 +4,29 @@ class MainClass
 {
     public static void Main(string[] args)
 
-    // ===================================== Задание 4.4.2
+    // ===================================== Задание 4.4.2 часть 2
+    //              Исходный код взят с финального здания модуля 3
     
     {
-        var anketa = (name: "", age: 0, birthdate: "");
+        //var anketa = (name: "", age: 0, birthdate: "");
+        // Более правильное задание кортеджа
+        //(string name, int age, string birthdate) anketa;
 
-        Console.Write("Enter your name: ");
-        anketa.name = Console.ReadLine().ToString();
+        // ещё одни вариант декларирования кортеджа
+        var (name, age, birthdate) = ("Dimon", 99, "32.13.2222");
 
-        Console.Write("Enter your age: ");
-        anketa.age = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("Enter your name: ");
+        //anketa.name = Console.ReadLine().ToString();
 
-        Console.WriteLine("Your name is {0} and age is {1}", anketa.name, anketa.age);
+        //Console.Write("Enter your age: ");
+        //anketa.age = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Enter your birthdate: ");
-        anketa.birthdate = Console.ReadLine();
+        Console.WriteLine("Your name is {0} and age is {1}", name, age);
 
-        Console.WriteLine("Your birthdate is " + anketa.birthdate);
+        //Console.Write("Enter your birthdate: ");
+        //anketa.birthdate = Console.ReadLine();
+
+        Console.WriteLine("Your birthdate is " + birthdate);
 
         //Console.WriteLine();
         //Console.Write(arr[i] + " ");
