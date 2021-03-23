efbae86ca800a5d56a03fb1bddd486bd7df8b4f7
DimVinich <DmitrijVS@gmail.com>
2021-03-23 09:17:52 +0300

    модуль 5 Введение 1

--- Modul5/Program.cs
+++ Modul5/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
-namespace Modul5
+class MainClass
 {
-    class Program
+    static void Main(string[] args)
+// ============================= модуль 5 Введение 1
     {
-        static void Main(string[] args)
+        ( string Name, string[] Dishes ) User;
+        User.Dishes = new string[5];
+
+        Console.WriteLine("Введите имя пользователя :");
+        User.Name = Console.ReadLine();
+
+        Console.WriteLine("Введите 5 любимых блюд пользователя");
+        for (int i = 1; i < User.Dishes.Length; i++ )
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Блюдо {i} :", i + 1);
+            User.Dishes[i] = Console.ReadLine();
         }
+
+        Console.ReadKey();
+
+        // Console.WriteLine(": {0}", name);
+        // Console.WriteLine(" "); 
+        //= Console.ReadLine();
+        //= Convert.ToInt32(Console.ReadLine());
     }
 }
