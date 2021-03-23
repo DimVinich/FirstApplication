b8d85fdffed721dd2c1eb93095fe4a7951d3ff84
DimVinich <DmitrijVS@gmail.com>
2021-03-23 09:59:37 +0300

    Задание 5.1.5

--- Modul5/Program.cs
+++ Modul5/Program.cs
@@ -2,22 +2,59 @@
 
 class MainClass
 {
-    static void Main(string[] args)
-// ============================= модуль 5 Введение 1
+    // ===================================== Задание 5.1.5
+
+    static string ShowColor()
     {
-        ( string Name, string[] Dishes ) User;
-        User.Dishes = new string[5];
+        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+        var color = Console.ReadLine();
 
-        Console.WriteLine("Введите имя пользователя :");
-        User.Name = Console.ReadLine();
-
-        Console.WriteLine("Введите 5 любимых блюд пользователя");
-        for (int i = 1; i < User.Dishes.Length; i++ )
+        switch (color)
         {
-            Console.WriteLine("Блюдо {i} :", i + 1);
-            User.Dishes[i] = Console.ReadLine();
+            case "red":
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+                break;
+
+            case "green":
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+                break;
+            case "cyan":
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+                break;
+            default:
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine("Your color is yellow!");
+                break;
         }
 
+        return color;
+    }
+
+    public static void Main(string[] args)
+    {
+        var favcolors = new string[3];
+
+        
+        for ( int i = 0; i < favcolors.Length; i++)
+        {
+            favcolors[i] = ShowColor();
+        }
+
+        for (int i = 0; i < favcolors.Length; i++)
+        {
+            Console.WriteLine("Номер: {0}   Цвет: {1}", i, favcolors[i]);
+        }
         Console.ReadKey();
 
         // Console.WriteLine(": {0}", name);
