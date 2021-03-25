bc30b7d844ad5337faff54482908bab6398ef6e0
DimVinich <DmitrijVS@gmail.com>
2021-03-25 17:32:24 +0300

    Задание 5.5.3

--- Modul5/Program.cs
+++ Modul5/Program.cs
@@ -2,135 +2,34 @@
 
 class MainClass
 {
-    // ===================================== Задание 5.3.13
+	// ================================================= Задание 5.5.3
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Напишите что-то");
+		var str = Console.ReadLine();
 
-    static int[] GetArrayFromConsole(int num = 5)              // ------------------ ввод массива интов
-    {
-        num = 5;
-        var result = new int[num];
+		Console.WriteLine("Укажите глубину эха");
+		var deep = int.Parse(Console.ReadLine());
 
-        for (int i = 0; i < result.Length; i++)
-        {
-            Console.WriteLine("Введите элемент массива номер {0}", i + 1);
-            result[i] = int.Parse(Console.ReadLine());
-        }
+		Echo(str, deep);
 
-        return result;
-    }
+		Console.ReadKey();
+	}
 
-    static void SortArray(in int[] array, out int[] sorteddesc, out int[] sortedasc)
-    {
-        sorteddesc = SortArrayDesc(array);
-        sortedasc = SortArrayAsc(array);
-    }
-    static int[] SortArrayAsc(int[] arrInt)                       // ------------------ сотрировка массива интов по возрастанию
-    {
+	static void Echo(string saidworld, int deep)
+	{
+		var modif = saidworld;
 
-        int minPrev = 0;
-        var l = arrInt.Length;               
-        for (int i = 0; i < l; i++)
-        {
-            for (int k = i + 1; k < l; k++)
-            {
-                if (arrInt[i] > arrInt[k])
-                {
-                    minPrev = arrInt[i];
-                    arrInt[i] = arrInt[k];
-                    arrInt[k] = minPrev;
-                }
-            }
-        }
-        return arrInt;
-    }
+		if (modif.Length > 2)
+		{
+			modif = modif.Remove(0, 2);
+		}
 
-   static int[] SortArrayDesc(int[] arrInt)                     // ------------------ сотрировка массива интов по убыванию
-    {
-        int minPrev = 0;
-        var l = arrInt.Length;
-        for (int i = 0; i < l; i++)
-        {
-            for (int k = i + 1; k < l; k++)
-            {
-                if (arrInt[i] < arrInt[k])
-                {
-                    minPrev = arrInt[i];
-                    arrInt[i] = arrInt[k];
-                    arrInt[k] = minPrev;
-                }
-            }
-        }
-        return arrInt;
-    }
+		Console.WriteLine("..." + modif);
 
-    static void ShowArray(int[] arrInt, bool sort = false)      // ------------------ вывод массива инто
-    {
-
-        for (int i = 0; i < arrInt.Length; i++)
-        {
-            Console.WriteLine(arrInt[i]);
-        }
-    }
-     
- //============================================================================
-    public static void Main(string[] args)
-    {
-        var array = GetArrayFromConsole(10);
-        ShowArray(array, true);
-
-
-        Console.ReadKey();
-
-        // Console.WriteLine(": {0}", name);
-        // Console.WriteLine(" "); 
-        //= Console.ReadLine();
-        //= Convert.ToInt32(Console.ReadLine());
-    }
-// ============================================================================
-
-    static void ShowColors(string username, params string[] favcolors)      // ---------------- отображение любымых цветов  
-    {
-        Console.WriteLine("Ваши любимые цвета:");
-        foreach (var color in favcolors)
-        {
-            Console.WriteLine(color);
-        }
-    }
-
-    static string ShowColor(string username, int userage)      // ---------------- заполнение любымых цветов  
-    {
-        Console.WriteLine("{0}, {1} лет\nНапишите свой любимый цвет на английском с маленькой буквы", username, userage);
-        var color = Console.ReadLine();
-
-        switch (color)
-        {
-            case "red":
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is red!");
-                break;
-
-            case "green":
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is green!");
-                break;
-            case "cyan":
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is cyan!");
-                break;
-            default:
-                Console.BackgroundColor = ConsoleColor.Yellow;
-                Console.ForegroundColor = ConsoleColor.Red;
-
-                Console.WriteLine("Your color is yellow!");
-                break;
-        }
-
-        return color;
-    }
-
+		if (deep > 1)
+		{
+			Echo(modif, deep - 1);
+		}
+	}
 }
