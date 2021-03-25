f50cb210cd9a859dbabba4c301267a98f8231898
DimVinich <DmitrijVS@gmail.com>
2021-03-25 21:33:42 +0300

    Задание 5.6

--- Modul5/Program.cs
+++ Modul5/Program.cs
@@ -1,35 +1,164 @@
 ﻿using System;
 
-class MainClass
+namespace UserAnketa
 {
-	// ================================================= Задание 5.5.3
-	public static void Main(string[] args)
-	{
-		Console.WriteLine("Напишите что-то");
-		var str = Console.ReadLine();
+    class Program
+    {
+        // ================================================= Задание 5.6
 
-		Console.WriteLine("Укажите глубину эха");
-		var deep = int.Parse(Console.ReadLine());
+        static void Main(string[] args)
+        {
+            (string name, string surname, int age, int fHavePet, int CountPet, string[] NamePet, int CountColor, string[] NameColor) anketa;
+            string YesPet;
+            string sEnter;
+            int iEnter;
+            bool fOkChek;
 
-		Echo(str, deep);
+            Console.Write("Введите имя: ");
+            anketa.name = Console.ReadLine();
 
-		Console.ReadKey();
-	}
+            Console.Write("Введите фамилию: ");
+            anketa.surname = Console.ReadLine();
 
-	static void Echo(string saidworld, int deep)
-	{
-		var modif = saidworld;
+            do
+            {
+                Console.Write("Введите возраст цифрами: ");
+                sEnter = Console.ReadLine();
 
-		if (modif.Length > 2)
-		{
-			modif = modif.Remove(0, 2);
-		}
+            } while (ChekNumber(sEnter, out iEnter) == false);
 
-		Console.WriteLine("..." + modif);
+            anketa.age = iEnter;
 
-		if (deep > 1)
-		{
-			Echo(modif, deep - 1);
-		}
-	}
+            anketa.fHavePet = 0;
+
+            do
+            {
+                Console.Write("Есть ли у вас питомцы?");
+                YesPet = Console.ReadLine();
+
+                if (YesPet.ToLower() == "yes" || YesPet.ToLower() == "да")
+                {
+                    anketa.fHavePet = 1;
+                    fOkChek = true;
+                }
+                else
+                {
+                    if (YesPet.ToLower() == "no" || YesPet.ToLower() == "нет")
+                    {
+                        anketa.fHavePet = 0;
+                        fOkChek = true;
+                    }
+                    else
+                    {
+                        fOkChek = false;
+                    }
+                }
+
+            } while (fOkChek == false);
+
+            string[] NameNull = new string[1];
+
+            NameNull[0] = "";
+
+            anketa.NamePet = NameNull;
+            anketa.CountPet = 0;
+
+            if (anketa.fHavePet == 1)
+            {
+                do
+                {
+                    Console.Write("Введите количество ваших питомцев: ");
+                    sEnter = Console.ReadLine();
+                } while (ChekNumber(sEnter, out iEnter) == false);
+
+                anketa.CountPet = iEnter;
+                anketa.NamePet = WhatNamePet(anketa.CountPet);
+            }
+
+            do
+            {
+                Console.Write("Сколько у вас любимых цветов? ");
+                sEnter = Console.ReadLine();
+            } while (ChekNumber(sEnter, out iEnter) == false);
+
+            anketa.CountColor = iEnter;
+
+            anketa.NameColor = NameNull;
+
+            anketa.NameColor = WhatFavoritColor(anketa.CountColor);
+
+
+            PrintDate((anketa));
+
+            Console.ReadKey();
+        }
+
+        static bool ChekNumber(string Snumber, out int Inumber)
+        {
+            if (Int32.TryParse(Snumber, out int intNumber))
+            {
+                if (intNumber > 0)
+                {
+                    Inumber = intNumber;
+                    return true;
+                }
+            }
+            {
+                Inumber = 0;
+                return false;
+            }
+        }
+
+        static string[] WhatNamePet(int kol)
+        {
+            string[] NamePet = new string[kol];
+
+            for (int i = 0; i < NamePet.Length; i++)
+            {
+                Console.WriteLine("Введите кличку питомца {0} : ", i + 1);
+                NamePet[i] = Console.ReadLine();
+            }
+
+            return NamePet;
+        }
+
+        static string[] WhatFavoritColor(int kol)
+        {
+            string[] NameColor = new string[kol];
+
+            for (int i = 0; i < NameColor.Length; i++)
+            {
+                Console.WriteLine("Введите цвет {0} : ", i + 1);
+                NameColor[i] = Console.ReadLine();
+            }
+
+            return NameColor;
+        }
+
+        static void PrintDate((string name, string surname, int age, int fHavePet, int CountPet, string[] NamePet, int CountColor, string[] NameColor) Fullanketa)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n");
+            Console.WriteLine(" -- Анкета --");
+            Console.WriteLine("Ваше имя: " + Fullanketa.name);
+            Console.WriteLine("Ваша Фамилия: " + Fullanketa.surname);
+            Console.WriteLine("Ваш возраст: " + Fullanketa.age);
+            Console.WriteLine("Наличие питомцев: " + (Fullanketa.fHavePet == 1 ? "Есть" : "Нет"));
+            if (Fullanketa.fHavePet == 1)
+            {
+                Console.WriteLine("\tУ вас {0} питомцев ", Fullanketa.CountPet);
+                Console.WriteLine("\tКлички ваших питомцев: ");
+                foreach (var Name in Fullanketa.NamePet)
+                {
+                    Console.WriteLine("\t\t" + Name);
+                }
+            }
+            Console.WriteLine("У Вас {0} любимых цветов: ", Fullanketa.CountColor);
+            foreach (var NColor in Fullanketa.NameColor)
+            {
+                Console.WriteLine("\t" + NColor);
+            }
+            Console.ResetColor();
+        }
+    }
 }
