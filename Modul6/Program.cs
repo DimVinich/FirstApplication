22b50f458a2afe344da63f8fe057cad901a984e0
DimVinich <DmitrijVS@gmail.com>
2021-04-05 20:20:34 +0300

    Модули 6 и 7

---
+++ Modul6/Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+
+namespace Modul6
+{
+	class User
+	{
+		private int age;
+		public int Age
+		{
+			get
+			{
+				return age;
+			}
+			set
+			{
+				if (value < 18)
+				{
+					Console.WriteLine("Возраст должен быть не меньше 18");
+				}
+				else
+				{
+					age = value;
+				}
+			}
+		}
+
+		private string eMail;
+		public string EMail
+        {
+			get 
+			{
+				return eMail;
+			}
+			set
+			{
+				if (value.Length < 3 || !value.Contains('@'))
+				{
+					Console.WriteLine("Не верно введён адрес иМайла");
+				}
+				else
+				{
+					eMail = value;
+				}
+			}
+		}
+
+		private string login;
+		public string Login
+		{ 
+			get
+            {
+				return login;
+            }
+			set
+            {
+				if (value.Length <= 3 )
+				{
+					Console.WriteLine("Не верно введён логин");
+				}
+				else
+				{
+					login = value;
+				}
+			}
+		}
+	}
+	class Circle
+	{
+		public double radius;
+
+		public double Square()
+		{
+		}
+
+		public double Length()
+		{
+		}
+	}
+	class Triangle
+	{
+		public int a;
+		public int b;
+		public int c;
+		public int A
+		{
+			get { return a; }
+			set
+			{
+				if (value > 0 && b + c > value)
+				{
+					a = value;
+				}
+			}
+		}
+		public int B
+		{
+			get { return b; }
+			set
+			{
+				if (value > 0 && a + c > value)
+				{
+					b = value;
+				}
+			}
+		}
+		public int C
+		{
+			get { return c; }
+			set
+			{
+				if (value > 0 && a + b > value)
+				{
+					c = value;
+				}
+			}
+		}
+		public double Square()
+		{
+		}
+		public double Perimeter()
+		{
+		}
+	}
+	class Square
+	{
+		public int side;
+
+		public double Square()
+		{
+		}
+
+		public double Perimeter()
+		{
+		}
+	}
+	enum TurnDirection
+	{
+		None = 0,
+		Left,
+		Right
+	}
+	class Car
+	{
+		private double Fuel;
+		private int Mileage;
+		private string color;
+		private TurnDirection turn;
+
+		public Car()
+		{
+			Fuel = 50;
+			Mileage = 0;
+			color = "White";
+		}
+
+		private void Move()
+		{
+			// Move a kilometer
+			Mileage++;
+			Fuel -= 0.5;
+		}
+
+		private void Turn(TurnDirection direction)
+		{
+			turn = direction;
+		}
+
+		public void FillTheCar()
+		{
+			Fuel = 50;
+		}
+
+		public string GetColor()
+		{
+			return color;
+		}
+
+		public void ChangeColor(string newColor)
+		{
+			if (color != newColor)
+				color = newColor;
+		}
+
+		public bool IsTurningLeft()
+		{
+			return turn == TurnDirection.Left;
+		}
+
+		public bool IsTurningRight()
+		{
+			return turn == TurnDirection.Right;
+		}
+	}
+
+	class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+ 
+    }
+}
+
---
+++ Modul7/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+
+namespace Modul7
+{
+
+
+	class Program
+    {
+        static void Main(string[] args)
+        {
+			D d = new D();
+			E e = new E();
+
+			d.Display();
+			((A)e).Display();
+			((B)d).Display();
+			((A)d).Display();
+			//Console.WriteLine("Hello World!");
+		}
+    }
+
+	class BaseClass
+	{
+		protected string Name;
+		public virtual int Counter
+		{
+			get;
+			set;
+		}
+		public BaseClass(string name)
+		{
+			Name = name;
+		}
+
+		public virtual void Display()
+        {
+			Console.WriteLine("Метод класса BaseClass");
+        }
+	}
+	class DerivedClass : BaseClass
+	{
+		public string Description;
+		private int counter;
+		public override int Counter
+        {
+            get
+            {
+				return counter;
+            }
+            set
+            {
+				if (value >= 0 )
+                {
+					counter = value;
+                }
+            }
+        }
+
+		public DerivedClass(string name, string description):base(name)
+        {
+			Description = description;
+        }
+
+		public DerivedClass(string name, string description, int counter):base(name)
+        {
+			Description = description;
+			Counter = counter;
+		}
+
+		//  Как вариант можно переписать второй конструктор
+		//public DerivedClass(string name, string description, int counter) : this(name, description)
+		//{
+		//	Counter = counter;
+		//}
+		public override void Display()
+		{
+			base.Display();
+			Console.WriteLine("Метод класса DerivedClass");
+		}
+
+	}
+
+
+	class Employee
+	{
+		public string Name;
+		public int Age;
+		public int Salary;
+	}
+	class ProjectManager : Employee
+	{
+		public string ProjectName;
+	}
+	class Developer : Employee
+	{
+		public string ProgrammingLanguage;
+	}
+
+	class Car
+	{
+		public int Mileage;
+
+		public Car()
+		{
+			Mileage = 0;
+		}
+
+		public virtual void Move()
+		{
+			Console.WriteLine("Вызван метод Move класса Car");
+			Mileage++;
+		}
+	}
+	enum FuelType
+	{
+		Gas = 0,
+		Electricity
+	}
+	class HybridCar : Car
+	{
+		public FuelType FuelType;
+		public double Gas;
+		public double Electricity;
+
+		public HybridCar()
+		{
+			Electricity = 50;
+			Gas = 50;
+		}
+
+		public override void Move()
+		{
+			Console.WriteLine("Вызван метод Move класса HybridCar");
+			Mileage++;     // правильней будет  base.Move(); !!
+
+			switch (FuelType)
+			{
+				case FuelType.Gas:
+					Gas -= 0.5;
+					break;
+				case FuelType.Electricity:
+					Electricity -= 0.5;
+					break;
+			}
+		}
+
+		public void ChangeFuelType(FuelType type)
+		{
+			FuelType = type;
+		}
+	}
+
+	class A
+    {
+		public virtual void Display() {
+			Console.WriteLine("A");
+		}
+    }
+	class B : A
+    {
+		public new void Display() {
+			Console.WriteLine("B");
+		}
+    }
+	class C : A
+    {
+        public override void Display()
+        {
+			Console.WriteLine("C");
+		}
+	}
+
+	class D : B
+    {
+		public new void Display(){
+			Console.WriteLine("D");
+		}
+	}
+
+	class E : C
+    {
+		public new void Display(){
+			Console.WriteLine("E");
+		}
+	}
+
+}
